07bf53657d219090f3bc899111486f3bb8a01720
Tasty-Pie <97690153+Tasty-Pie@users.noreply.github.com>
2024-02-27 09:56:40 +0600

    Update leetcode solutions.cpp

--- leetcode solutions.cpp
+++ leetcode solutions.cpp
@@ -1,4 +1,4 @@
-auto speedUp = []() { // The first variant of the cheat code with a lambda
+auto speedUp = []() { // The first variant of the cheat code with a lambda.
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
@@ -7,17 +7,17 @@ auto speedUp = []() { // The first variant of the cheat code with a lambda
 
 class Solution {
 public:
-    Solution() { // The second variant of the cheat code with a constructor
+    Solution() { // The second variant of the cheat code with a constructor.
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
     }
 
-    // 125. Valid Palindrome
-    bool isAlphanumeric(char letter) {
+    // 125. Valid Palindrome. Time = O(n).
+    bool isAlphanumeric(char letter) { // Simpler version.
         return ('a' <= letter && letter <= 'z') or ('A' <= letter && letter <= 'Z') or
-			('0' <= letter && letter <= '9'); // Simpler version
-    }
+			('0' <= letter && letter <= '9');
+	}
 
     bool isAlphanumeric(char letter) {
         if('a' <= letter && letter <= 'z') {
@@ -36,9 +36,9 @@ public:
         int l = 0, r = s.size() - 1;
         while(l < r) {
             if(not isAlphanumeric(s[l])) {
-                l++; // Skip non-alphanumeric chars
+                l++; // Skip non-alphanumeric chars.
             } else if(not isAlphanumeric(s[r])) {
-                r--; // Skip non-alphanumeric chars
+                r--; // Skip non-alphanumeric chars.
             } else {
                 if(tolower(s[l]) != tolower(s[r])) {
                     return false;
@@ -50,7 +50,7 @@ public:
         return true;
     }
 
-    // 1. Two Sum
+    // 1. Two Sum. Time = O(n*log(n)) for sorting.
     vector<int> twoSum(vector<int> &nums, int target) {
         vector<vector<int>> pairs(nums.size());
         for(int i = 0; i < nums.size(); i++) {
@@ -71,7 +71,7 @@ public:
         return {};
     }
 
-    // 11. Container With Most Water
+    // 11. Container With Most Water. Time = O(n) until two pointers become equal.
 	int maxArea(vector<int> &height) {
 		int i = 0, j = height.size() - 1;
 		int maxWater = 0;
@@ -92,8 +92,9 @@ public:
 		}
 		return maxWater;
 	}
-    // 875. Koko Eating Bananas
-    long findTime(vector<int> &piles, int speed) {
+
+    // 875. Koko Eating Bananas. Time = O(n*log(range)), where range ~ max value in the array piles.
+    long findTime(vector<int> &piles, int speed) { // O(n)
         long hours = 0;
         for(int bananas : piles) {
             hours += bananas / speed;
@@ -106,9 +107,9 @@ public:
 
     int minEatingSpeed(vector<int> &piles, long h) {
         int l = 1, r = *max_element(piles.begin(), piles.end());
-        while(l < r) {
+        while(l < r) { // log(range).
             int mid = l + (r - l) / 2;
-            long time = findTime(piles, mid);
+            long time = findTime(piles, mid); // O(n).
             if(time <= h) {
                 r = mid;
             } else {
@@ -117,8 +118,9 @@ public:
         }
         return l;
     }
-	// 206. Reverse Linked List
-	// Recursive solution
+
+	// 206. Reverse Linked List. Time = O(n).
+	// Recursive solution. Space = O(n) because of a call stack for recursive calls.
 	ListNode* helper(ListNode* head) {
         if(head->next == nullptr) {
             return head;
@@ -140,7 +142,8 @@ public:
         head->next = nullptr;
         return lastElem;
     }
-	// Loop solution
+
+	// Loop solution.
 	ListNode* reverseList(ListNode* head) {
         auto currentElement = head;
         ListNode* previousElement = nullptr;
@@ -153,20 +156,20 @@ public:
         return previousElement;
     }
 
-	// 53. Maximum Subarray
-	// Prefix sum solution
+	// 53. Maximum Subarray.
+	// Prefix sum solution. Time = O(n^2).
 	int maxSubArray(vector<int> &nums) {
         int n = nums.size();
         vector<int> prefixSum(n);
         prefixSum[0] = nums[0];
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i < n; i++) { // O(n).
             prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
 
         int maxSum = nums[0];
-        for(int l = 0; l < n; l++) {
-            for(int r = l; r < n; r++) {
-                int rangeSum = prefixSum[r] - (l == 0 ? 0 : prefixSum[l - 1]);
+        for(int l = 0; l < n; l++) { // O(n).
+            for(int r = l; r < n; r++) { // O(n).
+                int rangeSum = prefixSum[r] - (l == 0 ? 0 : prefixSum[l - 1]); // O(1).
                 if(rangeSum > maxSum) {
                     maxSum = rangeSum;
                 }
@@ -175,11 +178,11 @@ public:
         return maxSum;
     }
 
-	// Kadane's algo or DP solution
+	// Kadane's algorithm or DP solution. Bottom-up. Time = O(n).
     int maxSubArray(vector<int> &nums) {
         int n = nums.size();
         int maxSum = nums[0];
-        int maxSubarraySum = 0; // S[i]
+        int maxSubarraySum = 0; // S[i].
         for(int i = 0; i < n; i++) {
             if(maxSubarraySum <= 0) {
                 maxSubarraySum = nums[i];
@@ -192,4 +195,118 @@ public:
         }
         return maxSum;
     }
+
+	// 1464. Maximum Product of Two Elements in an Array. Time = O(n), Space = O(1).
+	int maxProduct(vector<int> &nums) {
+        priority_queue<int, vector<int>, greater<int>> pq; // Min heap.
+        for(int elem : nums) {
+            pq.push(elem);
+            if(pq.size() > 2) {
+                pq.pop();
+            }
+        }
+        int a = pq.top();
+        pq.pop();
+        return (a - 1) * (pq.top() - 1);
+    }
+
+	//1642. Furthest Building You Can Reach. Time = O(n*log(l)), Space = O(l), where l is a number of ladders.
+    int furthestBuilding(vector<int> &hs, int b, int l) {
+        int n = hs.size() - 1;
+        priority_queue<int, vector<int>, greater<int>> largestJmps;
+        int i = 0;
+        for(; i < n; i++) {
+            int delta = hs[i + 1] - hs[i];
+            if(delta <= 0) {
+                continue;
+            }
+            largestJmps.push(delta);
+            if(largestJmps.size() > l) {
+                int minVal = largestJmps.top();
+                if(minVal > b) {
+                    break;
+                } else {
+                    b -= minVal;
+                    largestJmps.pop();
+                }
+            }
+        }
+        return i;
+    }
+
+	// 215. Kth Largest Element in an Array.
+	// Sorting solution (slow). Time = O(n*log(n)).
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() - k];
+    }
+
+	// nth_element or Quick select solution. Time = O(n).
+    int findKthLargest(vector<int>& nums, int k) {
+        nth_element(nums.begin(), nums.end() - k, nums.end());
+        return nums[nums.size() - k];
+    }
+
+	// Priority queue solution. Time = O(n*log(k)). Space = O(k). Note: 0 < k <= n.
+    int findKthLargest(vector<int> &nums, int k) {
+        int n = nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq; // Min heap.
+        for(int elem : nums) {
+            pq.push(elem);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+
+	// 198. House Robber, Time = O(n)
+	// Bottom-up, constant space (Space = O(1))
+    int rob(vector<int> &nums) {
+        int n = nums.size();
+        if(n == 1) {
+            return nums[0];
+        }
+        int ms0 = nums[0], ms1 = max(nums[0], nums[1]); // ms - maximum sum for current index i
+        for(int i = 2; i < n; i++) {
+            int newMS = max(ms1, ms0 + nums[i]);
+            ms0 = ms1;
+            ms1 = newMS;
+        }
+        return ms1;
+    }
+
+	//Recursive DP, linear space for memo (Space = O(n))
+	vector<int> memo;
+
+    int helper(vector<int> &nums, int n) {
+        if(n == 1) {
+            return nums[0];
+        }
+        if(n == 2) {
+            return max(nums[0], nums[1]);
+        }
+        if(memo[n] == -1) {
+            memo[n] = max(helper(nums, n - 1), helper(nums, n - 2) + nums[n - 1]);
+        }
+        return memo[n];
+    }
+
+    int rob(vector<int> &nums) {
+        int n = nums.size();
+        memo = vector<int>(n + 1, -1);
+        return helper(nums, n);
+    }
+
+	// Bottom-up, use the input vector nums as a memo, constant extra space (Space = O(1)).
+	int rob(vector<int> &nums) {
+		if (nums.size() == 1) {
+			return nums[0];
+		}
+		nums[1] = max(nums[0], nums[1]);
+		for (int i = 2; i < nums.size(); i++) {
+			nums[i] = max(nums[i - 2] + nums[i], nums[i - 1]);
+		}
+		return nums.back();
+	}
 };
